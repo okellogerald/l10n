c39aeea706d5dfb1cdf949caba1c73c5d06b2f6c
Okello <okellogeralddev@gmail.com>
2023-11-06 10:18:45 +0300

    minor udpates

--- src/app/globals.go
+++ src/app/globals.go
@@ -4,10 +4,10 @@ var locales = []string{"en", "sw"}
 
 // will look for all files ending with *_en.arb and *_sw.arb in the folders
 // existing in this folder
-const from = "/Users/mac/Programs/Frontend/lipa_link/l10n"
+const from = "/Users/mac/Programs/Frontend/lipa_china/l10n"
 
 // will look for app_en.arb and app_sw.arb here
-const to = "/Users/mac/Programs/Frontend/lipa_link/l10n"
+const to = "/Users/mac/Programs/Frontend/lipa_china/l10n"
 
 // Will attempt to run 'flutter pub get' command once the translations are combined
-const FlutterProjectDir = "/Users/mac/Programs/Frontend/lipa_link/"
+const FlutterProjectDir = "/Users/mac/Programs/Frontend/lipa_china/"

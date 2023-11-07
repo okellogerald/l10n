fe136b983cd2066a349c7f6ef18fdae26a3477db
Okello <okellogeralddev@gmail.com>
2023-11-07 12:05:19 +0300

    clean up

--- src/app/globals.go
+++ src/app/globals.go
@@ -6,12 +6,9 @@ var Locales = []Locale{"en", "sw"}
 // Locale from which the descriptions and placeholders are going to be checked from
 var MainLocale = "en"
 
-// will look for all files ending with *_en.arb and *_sw.arb in the folders
+// will look for all files ending with *_en.json and *_sw.json in the folders
 // existing in this folder
 const FROM = "/Users/mac/Programs/Personal/l10n_flutter/l10n"
-
-// will look for app_en.arb and app_sw.arb here
-const TO = "/Users/mac/Programs/Personal/l10n_flutter/l10n"
 
 // Will attempt to run 'flutter pub get' command once the translations are combined
 const FlutterProjectDir = "/Users/mac/Programs/Personal/l10n_flutter"

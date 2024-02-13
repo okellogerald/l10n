d1dc78c50ce96849120dbebe56fb874dd3a9d9c1
Okello <okellogeralddev@gmail.com>
2024-02-13 16:51:19 +0300

    saved settings for tembo_auth

--- src/app/globals.go
+++ src/app/globals.go
@@ -7,11 +7,11 @@ var Locales = []Locale{"en", "sw"}
 const MainLocale = "en"
 
 // will look for localization files in this folder
-const FROM = "/Users/mac/Downloads/Programs/temboplus/lipa_china/l10n/"
+const FROM = "/Users/mac/Downloads/Programs/temboplus/tembo_auth/l10n/"
 
 // Will attempt to run 'flutter pub get' command once the translations are combined
-const FlutterProjectDir = "/Users/mac/Downloads/Programs/temboplus/lipa_china/"
+const FlutterProjectDir = "/Users/mac/Downloads/Programs/temboplus/tembo_auth/"
 
 // Where the generated localization files will be stored
-const LocalizationsDir = "/Users/mac/Downloads/Programs/temboplus/lipa_china/lib/localizations/"
+const LocalizationsDir = "/Users/mac/Downloads/Programs/temboplus/tembo_auth/lib/localizations/"
 

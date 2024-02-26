83394a96f9620ce102c1b20c5f7fc30934287142
Okello <okellogeralddev@gmail.com>
2024-02-26 14:03:28 +0300

    added settings for tembo_core

--- src/app/globals.go
+++ src/app/globals.go
@@ -7,11 +7,11 @@ var Locales = []Locale{"en", "sw"}
 const MainLocale = "en"
 
 // will look for localization files in this folder
-const FROM = "/Users/mac/Downloads/Programs/temboplus/tembo_app/l10n/"
+const FROM = "/Users/mac/Downloads/Programs/temboplus/tembo_core/l10n/"
 
 // Will attempt to run 'flutter pub get' command once the translations are combined
-const FlutterProjectDir = "/Users/mac/Downloads/Programs/temboplus/tembo_app/"
+const FlutterProjectDir = "/Users/mac/Downloads/Programs/temboplus/tembo_core/"
 
 // Where the generated localization files will be stored
-const LocalizationsDir = "/Users/mac/Downloads/Programs/temboplus/tembo_app/lib/localizations/"
+const LocalizationsDir = "/Users/mac/Downloads/Programs/temboplus/tembo_core/lib/localizations/"
 
